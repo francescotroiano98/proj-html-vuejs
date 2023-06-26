b6b29e0d7f7565e0a1133f0652dfa64b795e4e4b
francescotroiano98 <francesco-troiano-98@libero.it>
2023-06-26 12:43:06 +0200

    added navbar and dinamically populated

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,51 @@
 <template>
-    <div>
-        
-    </div>
+    <header>
+        <Navbar :menubar="menubar"/>
+    </header>
+    
 </template>
 <script>
+import Navbar from './Navbar.vue'
 export default {
-    name:"AppHeader"
+    data(){
+        return{
+            menubar: [
+        {
+          name: 'Home',
+          options: ['Home 1', 'Home 2', 'Home 3', 'Home 4', 'Home 5', 'Home 6']
+        },
+        {
+          name: 'About',
+          options: ['Home 1', 'Home 2', 'Home 3']
+        },
+        {
+          name: 'Academy',
+          options: []
+        },
+        {
+          name: 'Courses',
+          options: []
+        },
+        {
+          name: 'News',
+          options: []
+        }
+      ]
+    
+        }
+        
+
+    },
+    name:"AppHeader",
+    components:{
+        Navbar
+    }
 }
 </script>
-<style lang="scss">
-    
+<style lang="scss" scoped>
+
+    header{
+        height: 200px;
+        background-color: red;
+    }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="menutitle in menubar">{{ menutitle.name }}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name:"Navbar",
+    props:{
+        menubar:Object
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
